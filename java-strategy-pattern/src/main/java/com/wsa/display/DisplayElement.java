366240302deeef0ac76610f13b00608783f968df
singh.kulwant@gmx.com <singh.kulwant@gmx.com>
2019-10-13 01:59:00 +0530

    12 Oct impl java inbuilt observer pattern

---
+++ java-strategy-pattern/src/main/java/com/wsa/display/DisplayElement.java
@@ -0,0 +1,6 @@
+package com.wsa.display;
+
+public interface DisplayElement {
+
+	public void display();
+}
---
+++ java-strategy-pattern/src/main/java/com/wsa/observer/Observer.java
@@ -0,0 +1,6 @@
+package com.wsa.observer;
+
+public interface Observer {
+
+	public void update(float temp, float humidity, float pressure);
+}
